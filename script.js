9f7276286e5103610910184c48d897dee265f20e
milmil2024 <167744298+milmil2024@users.noreply.github.com>
2024-04-23 11:22:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get the button and the image container
+const button = document.getElementById('horrorButton');
+const imageContainer = document.getElementById('horrorImageContainer');
+
+// Add click event listener to the button
+button.addEventListener('click', function() {
+    // Show the horror image container
+    imageContainer.style.display = 'block';
+});
